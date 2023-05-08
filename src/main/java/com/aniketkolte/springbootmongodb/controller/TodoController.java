2cdfd957afe099739853abfa3a067c5c21904236
aniketthink <aniket.kolate@thinkitive.com>
2023-05-08 15:26:45 +0530

    feat: Implement createTodo api

--- src/main/java/com/aniketkolte/springbootmongodb/controller/TodoController.java
+++ src/main/java/com/aniketkolte/springbootmongodb/controller/TodoController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,5 +25,16 @@ public class TodoController {
         if (todo.size() > 0)
             return new ResponseEntity<List<TodoDTO>>(todo, HttpStatus.OK);
         return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/todos")
+    public  ResponseEntity<?> createTodo(@RequestBody TodoDTO todo){
+        try {
+                todo.setCreatedAt(new Date(System.currentTimeMillis()));
+                todoRepo.save(todo);
+                return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
