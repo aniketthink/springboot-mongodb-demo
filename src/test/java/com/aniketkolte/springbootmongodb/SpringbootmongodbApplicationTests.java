510ea4551c5c9f539c29e714ba5c7f22fd1f51bc
aniketthink <aniket.kolate@thinkitive.com>
2023-05-08 12:22:25 +0530

    initial commit

---
+++ src/test/java/com/aniketkolte/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniketkolte.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
