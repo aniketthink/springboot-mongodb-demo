f55a2d9e03849fb12f94f757bbd050eede966875
aniketthink <aniket.kolate@thinkitive.com>
2023-05-08 16:56:29 +0530

    feat: Add ValidationConfig class

--- src/main/java/com/aniketkolte/springbootmongodb/model/TodoDTO.java
+++ src/main/java/com/aniketkolte/springbootmongodb/model/TodoDTO.java
@@ -1,5 +1,6 @@
 package com.aniketkolte.springbootmongodb.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,10 +14,13 @@ public class TodoDTO {
     @Id
     private String id;
 
+    @NotNull(message = "todo cannot be null")
     private  String todo;
 
+    @NotNull(message = "description cannot be null")
     private String description;
 
+    @NotNull(message = "completed cannot be null")
     private boolean completed;
 
     private Date createdAt;
